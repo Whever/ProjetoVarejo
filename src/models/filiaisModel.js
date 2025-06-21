7d3eb12426d3a1e58249bdb2e689d94333448880
unknown <jgabrieladpk@gmail.com>
2025-06-21 14:19:51 -0300

    filiais model

---
+++ src/models/filiaisModel.js
@@ -0,0 +1,29 @@
+const { sequelize } = require('../config/db');
+const { DataTypes } = require('sequelize')
+
+const filialModel = sequelize.define('filiais',{
+    ID_Filial: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nomeFilial: {
+       type: DataTypes.STRING,
+       allowNull: false
+    },
+     enderecoFilial: {
+        type: DataTypes.STRING,
+        allowNull: false,
+     },
+     cidadeFilial: {
+        type:DataTypes.STRING,
+        allowNull: false,
+     }
+},{
+      tableName: 'Filiais',
+      timestamps: false
+
+    });
+
+module.exports = {filialModel}
+
