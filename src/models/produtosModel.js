0ad018596a05af93482a4f02e85cbcb642c6d619
Wheverton <wheverton.firmino@gmail.com>
2025-06-21 14:14:07 -0300

    adição PedidosModel

--- src/models/produtosModel.js
+++ src/models/produtosModel.js
@@ -1,23 +1,23 @@
-const {sequelize} = require('../config/db'); 
-const {DataTypes} = require('sequelize');
+const { sequelize } = require('../config/db');
+const { DataTypes } = require('sequelize');
 
-const produtosModel = sequelize.define("produtos",{
-    idProduto:{
-        type:DataTypes.INTEGER,
-        autoincrement:true,
-        primarykey:true
+const produtosModel = sequelize.define("produtos", {
+    idProduto: {
+        type: DataTypes.INTEGER,
+        autoincrement: true,
+        primarykey: true
     },
-    nome:{
-        type:DataTypes.STRING,
-        allownull:false
-    },valorProduto:{
-        type:DataTypes.DECIMAL(10,2),
-        allownull:false
-    },quantidadeProduto:{
-        type:DataTypes.INTEGER,
-        allownull:false
+    nome: {
+        type: DataTypes.STRING,
+        allownull: false
+    }, valorProduto: {
+        type: DataTypes.DECIMAL(10, 2),
+        allownull: false
+    }, quantidadeProduto: {
+        type: DataTypes.INTEGER,
+        allownull: false
     }
-},{
-    tabelname:"produtos",
-    timeStamps:false
+}, {
+    tabelname: "produtos",
+    timestamps: false
 })
