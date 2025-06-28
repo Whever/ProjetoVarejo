9ce55c06a17ca182b55b1030c7d977d26777fe7f
unknown <joaofiais2500@gmail.com>
2025-06-28 10:33:26 -0300

    porta varejo

--- src/routes/clienteRouter.js
+++ src/routes/clienteRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const Router = express.Router(); // ele instancia o router, um objeto do express para definir rotas.
 
 
-Router.get ("/", filialController.listarClientes) // esta rota ira listar os clientes.
+Router.get ("/", clienteController.listarClientes) // esta rota ira listar os clientes.
 
-router.post ("/", filialController.cadastrarClientes) // esta rota é responsavel por cadastrar os clientes.
+router.post ("/", clienteController.cadastrarClientes) // esta rota é responsavel por cadastrar os clientes.
 
-router.put ("/:ID_Filial", filialController.atualizadaClientes) // esta rota ira atualizar qualquer alteraçao em algum cliente.
+router.put ("/:ID_Filial", clienteController.atualizadaClientes) // esta rota ira atualizar qualquer alteraçao em algum cliente.
 
-router.delete("/:ID_Filial", filialController.deletarClientes) // esta rota é responsavel por deletar qualquer cliente.
+router.delete("/:ID_Filial", clienteController.deletarClientes) // esta rota é responsavel por deletar qualquer cliente.
 
 
