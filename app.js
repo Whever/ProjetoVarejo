67030327d366471d40ec05a46750826deb1bbe33
unknown <jgabrieladpk@gmail.com>
2025-06-28 17:35:49 -0300

    controller

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const APP = express(); // criar uma intancia do express, armazenando tudo no "app"
 
 const PORT = 8081; // criar a porta
-
+const {estoqueRoute} = require('./src/controller/estoqueContoller')
 app.use(express.json()); // configura o body - parser para interpretar corpo de requisiçao no formato json.
+
+app.use('/',estoqueRoute)
 
 app.listen(PORT, ()=> {
     console.log(`servidor esta rodando na porta: ${PORT}`)
--- src/routes/estoquesRoute.js
+++ src/routes/estoquesRoute.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const { estoqueController } = require('../controller/estoqueContoller');
 const router = express.Router();// Instancia o objeto do express usado para definir rotas
 
 //Rotas de Filiais
-router.get ("/", async (req, res)=>{
-    res.send("Coleta Estoque");
-});// Rota responsável por listar o estoque no sistema
+//router.get ("/", async (req, res)=>{
+    //res.send("Coleta Estoque");
+//});// Rota responsável por listar o estoque no sistema
 
-router.post ("/", async (req, res)=>{
-    res.send("Cadastrar Estoque");
-});// Rota responsável por criar as filiais do sistema
+//router.post ("/", async (req, res)=>{
+   // res.send("Cadastrar Estoque");
+//});// Rota responsável por criar as filiais do sistema
 
-router.put ("/", async (req, res)=>{
-    res.send("Atualizar Estoque");
-});// Rota responsável por atualizar as filiais do sistema
+//router.put ("/", async (req, res)=>{
+   // res.send("Atualizar Estoque");
+//});// Rota responsável por atualizar as filiais do sistema
 
-router.delete ("/", async (req, res)=>{
-    res.send("Deletar Estoque");
-});// Rota responsável por deletar as filiais do sistema
+//router.delete ("/", async (req, res)=>{
+   // res.send("Deletar Estoque");
+//});// Rota responsável por deletar as filiais do sistema
 
 module.exports = { rotasEstoque: router };// Exporta o objeto de rotas para ser usado em outros arquivos
+
+router.get ("/", estoqueController.listarEstoque);
+
+router.post("/",estoqueController.cadastrarEstoque);
+
+router.put("/",estoqueController.atualizarEstoque);
+
+router.delete("/", estoqueController.deletarEstoque);
+
+module.exports ={estoqueRouter : router}
