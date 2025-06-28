7c10f42d5aa31841a5f636c38ffd0a94f396691c
unknown <joaofiais2500@gmail.com>
2025-06-28 11:31:57 -0300

    rotas filiais

--- src/routes/clienteRouter.js
+++ src/routes/clienteRouter.js
@@ -2,12 +2,20 @@ const express = require('express');
 const Router = express.Router(); // ele instancia o router, um objeto do express para definir rotas.
 
 
-Router.get ("/", clienteController.listarClientes) // esta rota ira listar os clientes.
+Router.get ("/", async (req, res)=>{
+    res.send("Coleta clientes");
+}) // esta rota ira listar os clientes.
 
-router.post ("/", clienteController.cadastrarClientes) // esta rota é responsavel por cadastrar os clientes.
+router.post ("/", async (req, res)=>{
+    res.send("cadastrar clientes")
+}); // esta rota é responsavel por cadastrar os clientes.
 
-router.put ("/:ID_Filial", clienteController.atualizadaClientes) // esta rota ira atualizar qualquer alteraçao em algum cliente.
+router.put ("/:ID_clientes", async (req, res)=>{
+    res.send("atualizar clientes")
+}); // esta rota ira atualizar qualquer alteraçao em algum cliente.
 
-router.delete("/:ID_Filial", clienteController.deletarClientes) // esta rota é responsavel por deletar qualquer cliente.
+router.delete("/:ID_clientes", async (req, res)=>{
+    res.send("deletar clientes")
+}); // esta rota é responsavel por deletar qualquer cliente.
 
 
