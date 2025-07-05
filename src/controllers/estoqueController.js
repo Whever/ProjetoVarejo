f329e2f5f8f77ee9872d194264ea6fabe2406963
Wheverton <wheverton.firmino@gmail.com>
2025-07-05 15:16:42 -0300

    estoque listar e deletar funcionando

---
+++ src/controllers/estoqueController.js
@@ -0,0 +1,129 @@
+const { estoqueModel } = require('./../models/estoqueModel');
+const { Op, where } = require('sequelize');
+const { parseData } = require('./../utils/dateUtils'); // Importa a função parseData para formatar datas
+
+const estoqueController = {
+    listarEstoque: async (req, res) => {
+
+        try {
+            const { ID_Estoque } = req.query;
+            let conditions = {};
+
+            if (ID_Estoque) {
+                conditions.ID_Estoque = ID_Estoque;
+            }
+
+            let estoques = await estoqueModel.findAll({
+                where: {
+                    [Op.or]: [
+                        { ID_Estoque: { [Op.eq]: conditions.ID_Estoque } }
+                    ]
+                }
+            });
+
+            estoques = estoques.map(estoque => {
+                return estoque;
+            });
+
+            return res.status(200).json(estoques);
+
+        } catch (error) {
+            console.error("Erro ao listar no estoque:", error);
+            return res.status(500).json({ message: "Erro ao listar no estoque" });
+        }
+    },
+    cadastrarEstoque: async (req, res) => {
+
+        try {
+
+            const { dataEntradaEstoque, dataSaida, statusEstoque, ID_FilialEstoque, ID_ProdutosEstoque } = req.body;
+            if (!dataEntradaEstoque || !dataSaida || !statusEstoque || !ID_FilialEstoque || !ID_ProdutosEstoque) {
+                return res.status(400).json({ message: "campos obrigatorios não preenchidos" })
+            }
+
+            let estoque = await estoqueModel.findOne({
+                where: {
+                    [Op.or]: [
+                        { ID_FilialEstoque },
+                    ]
+                }
+            });
+
+            if (estoque) {
+                return res.status(409).json({ message: "já cadastrado no estoque!" });
+            }
+
+            await estoqueModel.create({
+                dataEntradaEstoque,
+                dataSaida,
+                statusEstoque,
+                ID_FilialEstoque,
+                ID_ProdutosEstoque
+            });
+
+            return res.status(201).json({ message: "cadastrado no estoque com sucesso!" })
+        } catch (error) {
+
+            console.error("Erro ao cadastrar no estoque:", error);
+            return res.status(500).json({ message: "Erro ao cadastrar no estoque!" })
+
+        }
+    },
+    atualizarEstoque: async (req, res) => {
+
+        try {
+
+            const { ID_Estoque } = req.params
+            const { dataEntradaEstoque,dataSaida,statusEstoque,ID_FilialEstoque,ID_ProdutosEstoque } = req.body;
+          
+
+            let estoque = await estoqueModel.findByPk(ID_Estoque);
+
+            if (dataEntradaEstoque||dataSaida||statusEstoque||ID_FilialEstoque||ID_ProdutosEstoque) {
+                return res.status(404).json({ message: "não encontrado no estoque!" });
+            }
+
+            let dadosAtualizados = { dataEntradaEstoque,dataSaida,statusEstoque,ID_FilialEstoque,ID_ProdutosEstoque };
+
+            await estoqueModel.update(dadosAtualizados, { where: { ID_Estoque } });
+
+            estoque = await estoqueModel.findByPk(ID_Estoque);
+
+            //return res.status(200).json({ message: "Atualizado no estoque com sucesso:", Estoque: estoque });
+
+        } catch (error) {
+
+            console.error("Erro ao atualizar no estoque:", error);
+            return res.status(500).json({ message: "Erro ao atualizar no estoque" });
+
+        }
+    },
+
+
+    deletarEstoque: async (req, res) => {
+
+        try {
+            const { ID_Estoque } = req.params
+            const estoque = await estoqueModel.findByPk(ID_Estoque)
+
+            if (!estoque) {
+                return res.status(404).json({ message: "não encontrado no estoque!" });
+            }
+            let result = await estoqueModel.destroy({
+                where: { ID_Estoque }
+            })
+            if (result > 0) {
+                return res.status(200).json({ message: `${statusEstoque} foi excluido no estoque com sucesso!` });
+            } else {
+                return res.status(404).json({ message: "Erro ao excluir no estoque!" });
+            }
+        }
+
+        catch (error) {
+            console.error("Erro ao excluir no estoque:", error);
+            return res.status(500).json({ message: "Erro ao excluir no estoque" });
+        }
+    }
+}
+
+module.exports = { estoqueController };
--- src/routes/estoquesRoute.js
+++ src/routes/estoquesRoute.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();// Instancia o objeto do express usado para definir rotas
 
+const {estoqueController} = require('./../controllers/estoqueController');// Importa o controlador de estoque
 //Rotas de Filiais
-router.get("/", async (req, res)=>{
-    res.send("Coleta Estoque");
-});// Rota responsável por listar o estoque no sistema
+router.get("/",estoqueController.listarEstoque );// Rota responsável por listar o estoque no sistema
 
-router.post ("/", async (req, res)=>{
-    const {ID_Estoque, dataEntradaEstoque, saidaEstoque, ID_FilialEstoque,ID_ProdutosEstoques } = req.body; // Extrai os dados do corpo da requisição
-    console.log(`ID Estoque: ${ID_Estoque}, Data Entrada: ${dataEntradaEstoque}, Saída: ${saidaEstoque}, ID Filial: ${ID_FilialEstoque}, ID Produtos: ${ID_ProdutosEstoques}`); // Exibe os dados no console
-    res.send("Cadastrar Estoque");
-});// Rota responsável por criar as filiais do sistema
+router.post ("/",estoqueController.cadastrarEstoque);// Rota responsável por criar as filiais do sistema
 
-router.put ("/:ID_Estoque", async (req, res)=>{
-    res.send("Atualizar Estoque");
-});// Rota responsável por atualizar as filiais do sistema
+router.put ("/:ID_Estoque", estoqueController.atualizarEstoque);// Rota responsável por atualizar as filiais do sistema
 
-router.delete ("/:ID_Estoque", async (req, res)=>{
-    res.send("Deletar Estoque");
-});// Rota responsável por deletar as filiais do sistema
+router.delete ("/:ID_Estoque",estoqueController.deletarEstoque);// Rota responsável por deletar as filiais do sistema
 
 module.exports = { estoqueRouter: router };// Exporta o objeto de rotas para ser usado em outros arquivos
