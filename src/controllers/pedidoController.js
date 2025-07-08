4b931118b36eca871f93711239193a3bc1f45640
Whever <wheverton.firmino@gmail.com>
2025-07-08 11:03:33 -0300

    Rota e controller vinculado leiura,cadastro,atualizaçãop funcionando verificar erro delete motivo pedindo realizar truncate

---
+++ src/controllers/pedidoController.js
@@ -0,0 +1,138 @@
+const { Op, where } = require('sequelize');
+const { pedidosModel } = require('./../models/pedidosModel');
+const { Where } = require('sequelize/lib/utils');
+
+const pedidoController = {
+
+    listarPedido: async (req, res) => {
+        try {
+            const { ID_Pedido } = req.query;
+            let conditions = {};
+
+            if (ID_Pedido) {
+                conditions.ID_Pedido = ID_Pedido;
+            }
+
+            let pedidos = await pedidosModel.findAll({
+                where: {
+                    [Op.or]: [
+                        { ID_Pedido: { [Op.eq]: conditions.ID_Pedido } }
+                    ]
+                }
+            })
+
+            pedidos = pedidos.map(pedido => {
+                return pedido;
+            });
+
+            return res.status(200).json({ pedidos });
+
+        } catch (error) {
+            console.error("Erro ao listar pedidos:", error);
+            return res.status(500).json({ message: "Erro ao listar pedidos" });
+        }
+
+    },
+
+    cadastrarPedido: async (req, res) => {
+        try {
+            const { DataPedido, StatusPedido, ValorPedido, ID_clienteProduto } = req.body;
+
+
+
+            if (!DataPedido || !StatusPedido || !ValorPedido || !ID_clienteProduto) {
+                return res.status(400).json({ message: "Campos obrigatórios não preenchidos" });
+            }
+
+            let produtoExiste = await pedidosModel.findOne({
+                where: {
+                    [Op.and]: [
+                        { DataPedido: DataPedido },
+                        { ID_clienteProduto: ID_clienteProduto },
+                    ]
+                }
+            });
+
+            if (produtoExiste) {
+                return res.status(409).json({ message: "Pedido já cadastrado!" });
+            }
+
+            await pedidosModel.create({
+                DataPedido: DataPedido,
+                StatusPedido: StatusPedido,
+                ValorPedido: ValorPedido,
+                ID_clienteProduto: ID_clienteProduto,
+            });
+            res.status(201).json({ message: "Pedido cadastrado com sucesso!" });
+
+        } catch (error) {
+            console.error("Erro ao cadastrar pedido:", error);
+            return res.status(500).json({ message: "Erro ao cadastrar pedido" });
+        }
+    },
+
+    atualizarPedido: async (req, res) => {
+
+        try {
+            const { ID_Pedidos } = req.params;
+            const { DataPedido, StatusPedido, ValorPedido, ID_clienteProduto } = req.body
+
+            if (!DataPedido || !StatusPedido || !ValorPedido || !ID_clienteProduto) {
+                return res.status(400).json({ message: "Campos obrigatórios não preenchidos" });
+            }
+
+            let pedido = await pedidosModel.findByPk(ID_Pedidos);
+
+            if (!pedido) {
+                return res.status(404).json({ message: "Pedido não encontrado" });
+            }
+
+            let dadosAtualizados = {
+                DataPedido: DataPedido,
+                StatusPedido: StatusPedido,
+                ValorPedido: ValorPedido,
+                ID_clienteProduto: ID_clienteProduto
+            }
+
+            await pedido.update(dadosAtualizados);
+            pedido = await pedidosModel.findByPk(ID_Pedidos);
+            return res.status(200).json({ message: "Pedido atualizado com sucesso!", pedido });
+        } catch (error) {
+            console.error("Erro ao atualizar pedido:", error);
+            return res.status(500).json({ message: "Erro ao atualizar pedido" });
+        }
+    },
+
+    deletarPedido: async (req, res) => {
+        try {
+            const { ID_Pedidos } = req.params;
+            
+            const pedido = await pedidosModel.findByPk(ID_Pedidos);
+
+            if (!pedido) {
+                return res.status(404).json({ message: "Pedido não encontrado" });
+            }
+
+            
+            // resolver problema erro -> Missing where or truncate attribute in the options parameter of model.destroy
+            
+
+            let result = await pedidosModel.destroy({
+                Where:{ID_Pedidos}           
+            });    
+
+            if(result > 0) {
+                return res.status(200).json({ message: "Pedido deletado com sucesso!" });
+            }else{
+                return res.status(404).json({ message: "Pedido não encontrado para deletar" });
+            }
+
+        } catch (error) {
+            console.error("Erro ao deletar pedido:", error);
+            return res.status(500).json({ message: "Erro ao deletar pedido" });
+        }
+    }
+
+}
+
+module.exports = { pedidoController };
